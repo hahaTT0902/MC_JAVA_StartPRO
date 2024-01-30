dbac6c8a33f68eb3f30fc09e8971511906337391
hahaTT <104051227+hahaTT0902@users.noreply.github.com>
2024-01-30 21:47:33 +0800

    Add files via upload

---
+++ Linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+while [ true ]; do
+    total_memory=$(free -m | awk '/^Mem:/{print $2}')
+
+    if [ $total_memory -le 12288 ]; then
+        java_memory=$(($total_memory / 2))
+        java_params="-XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5"
+    else
+        java_memory=16384
+        java_params="-XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+    fi
+
+    java -Xmx${java_memory}M -Xms${java_memory}M $java_params -jar server.jar --nogui
+
+    echo ...服务器正在重启...
+    echo 按CTRL+C以重启
+done
